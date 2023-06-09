c9126187f5a2cfe8b34ffcd537b63a8a83167e04
bhavana1308 <bonamukkala.bhavana@gmail.com>
2023-06-08 21:18:24 -0500

    initial commit

--- src/main/java/org/solvd/com/dao/service/CartListDao.java
+++ src/main/java/org/solvd/com/dao/service/CartListDao.java
@@ -5,20 +5,24 @@ import org.solvd.com.dao.model.CartList;
 import org.solvd.com.dao.utils.AbstractDao;
 import org.solvd.com.dao.utils.ConnectionFactory;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CartListDao extends AbstractDao<CartList> {
 
-    private final String SELECT_BY_ID= "SELECT * FROM CartList WHERE cartId = cartId ;" ;
+    private final String SELECT_BY_ID = "SELECT * FROM CartList WHERE cartId = ? ;";
 
     private final String DELETE_QUERY = " DELETE FROM CartList WHERE cartId = ? ;";
 
     public CartList findById(int cartId) {
         CartList cartList = new CartList();
         try (Connection connection = ConnectionFactory.getConnection();
-             Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(SELECT_BY_ID);
+             PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID)) {
+            statement.setInt(1, cartId);
+            ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 cartList = getCartListFromResultSet(resultSet);
             } else {
